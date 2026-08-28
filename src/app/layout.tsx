128ec49bb6e75c53098980707b853ebd64e9b4f4
Adam Pogash <minipog@gmail.com>
2026-08-28 18:55:11 -0400

    chore: configure Google Tag Manager container GTM-PB9D9RHS in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -417,7 +417,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KBWXBRFV');`}
+})(window,document,'script','dataLayer','GTM-PB9D9RHS');`}
         </Script>
         {/* End Google Tag Manager */}
         {/* Official LLM & AI Assistant Discovery Links (llms.txt standard) */}
@@ -440,7 +440,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KBWXBRFV"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PB9D9RHS"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
